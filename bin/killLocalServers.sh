67fe2876e60795d9a8777cd14219471c0fe4d162
Esteban <lewisesteban@hotmail.fr>
2020-02-17 12:11:23 +0100

    killlocalservers with and without space

--- bin/killLocalServers.sh
+++ bin/killLocalServers.sh
@@ -4,3 +4,10 @@ do
 echo "Killing $line"
 kill $line
 done
+
+lines=$(ps axo pid,cmd | grep "paxos_server.jar" | grep "java -jar" | grep -v "grep" | grep -Eo '^[^ ]+')
+for line in $lines
+do
+echo "Killing $line"
+kill $line
+done
