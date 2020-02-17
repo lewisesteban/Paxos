181931815cc4d73690c32ec01e6a8228849ec593
Esteban <lewisesteban@hotmail.fr>
2020-02-17 11:58:14 +0100

    kilLocalServers beginning space

--- bin/killLocalServers.sh
+++ bin/killLocalServers.sh
@@ -1,4 +1,4 @@
-lines=$(ps axo pid,cmd | grep "paxos_server.jar" | grep "java -jar" | grep -v "grep" | grep -Eo '^[^ ]+')
+lines=$(ps axo pid,cmd | grep "paxos_server.jar" | grep "java -jar" | grep -v "grep" | grep -Eo '^ [^ ]+')
 for line in $lines
 do
 echo "Killing $line"
